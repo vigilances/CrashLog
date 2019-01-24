b8e625c4906c841ceba5d2b38cad5bea781c09b6
vigilances <vigilance.zx@gmail.com>
2019-01-24 16:52:27 +0800

    Update MainActivity.java
    calculate age

--- app/src/main/java/com/miracle/crashlog/MainActivity.java
+++ app/src/main/java/com/miracle/crashlog/MainActivity.java
@@ -28,4 +28,50 @@ public class MainActivity extends AppCompatActivity {
             }
         });
     }
+    
+     /**
+     * 年龄计算
+     * 0--传入时间戳   1--传入日期格式字符串("2012-12-2")
+     *
+     * @param time
+     * @return
+     */
+    public static String calculateAge(int src, String time) {
+        if (TextUtils.isEmpty(time)) {
+            return "0";
+        }
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+        Date birthday = null;
+        try {
+            if (src == 0) {
+                birthday = sdf.parse(stampToDate(time));
+            } else {
+                birthday = sdf.parse(time);
+            }
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+        Calendar now = Calendar.getInstance();
+        Calendar b = Calendar.getInstance();
+        b.setTime(birthday);
+        int year = now.get(Calendar.YEAR) - b.get(Calendar.YEAR);
+        int month = now.get(Calendar.MONTH) - b.get(Calendar.MONTH);
+        int day = now.get(Calendar.DAY_OF_MONTH) - b.get(Calendar.DAY_OF_MONTH);
+        if (month < 0) {
+            month = 12 - b.get(Calendar.MONTH) + now.get(Calendar.MONTH);
+            year -= 1;
+        }
+        if (day < 0) {
+            day = b.getMaximum(Calendar.DAY_OF_MONTH) - b.get(Calendar.DAY_OF_MONTH) + now.get(Calendar.DAY_OF_MONTH);
+            month -= 1;
+        }
+        if (year >= 1) {
+            return year + "岁";
+        } else {
+            if (month < 1) {
+                return day + "天";
+            }
+            return month + "个月";
+        }
+    }
 }
